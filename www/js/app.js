ea56ee375f0bbaa58f772b8c68384688145d6f0c
yogesh <yogesh@GIFTECH170-PC.giis.com>
2016-07-04 16:24:26 +0530

    1.0.4

---
+++ www/js/app.js
@@ -0,0 +1,35 @@
+angular.module('starter', ['ionic', 'starter.controllers','ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+          url: '/app',
+          abstract: true,
+          templateUrl: 'templates/app.html'
+      })
+    .state('app.dashboard', {
+        url: '/dashboard',
+        views: {
+            'appContent': {
+                templateUrl: 'templates/dashboard.html'
+            }
+        }
+    })
+    .state('logIn', {
+        url: '/logIn',
+        templateUrl: 'templates/logIn.html',
+        controller: "loginCtr"
+    });
+
+    $urlRouterProvider.otherwise('/logIn');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', [])
+.controller("loginCtr", ["$scope", "$state", "$ionicViewService", function ($scope, $state, $ionicViewService) {
+    var home = "app.dashboard";
+    $scope.doLogIn = function () {
+        $ionicViewService.nextViewOptions({disableBack: true});
+        $state.go(home, {}, { location: 'replace' });
+    }
+}]);
